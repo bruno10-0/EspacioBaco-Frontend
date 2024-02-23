d61f7765e3eca9e07f6630de90d85f909b28786e
Bruno <brunobrodon75@gmail.com>
2024-02-23 14:35:09 -0300

    feat: :sparkles: Agregamos un slider de Cards atractivo

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,7 +2,7 @@ import { FaInstagram,FaWhatsapp,FaFacebookF   } from "react-icons/fa6";
 export const Footer = () => {
     return (
         <>
-            <footer className="footer footer-center p-10 mt-5 bg-base-100 text-base-content rounded">
+            <footer className="footer footer-center p-5 mt-56 bg-base-100 text-base-content rounded">
                 <nav className="grid grid-flow-col gap-4">
                     <a className="link link-hover">About us</a>
                     <a className="link link-hover">Contact</a>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,7 @@
 import { NavBar } from "../components/navBar/navBar"
 import { SliderAuto } from "../components/sliderAuto/sliderAuto"
 import { Footer } from "../components/footer/footer";
+import {SiliderMobile} from "./../components/siliderMobile/siliderMobile"
 export const Home = () => {
     const carouselItems = [
         { imageUrlLarge: 'https://www.solyvinomendoza.com/img/cms/Blend%20Tintos_Web%20pc.jpg', imageUrlSmall: "https://www.solyvinomendoza.com/img/cms/Blend%20Tintos_Web%20Mobil.jpg", altText: 'VinoMalbec' },
@@ -8,9 +9,10 @@ export const Home = () => {
         { imageUrlLarge: 'https://www.solyvinomendoza.com/img/cms/Malbec%2010off_Web%20pc.jpg', imageUrlSmall: "https://www.solyvinomendoza.com/img/cms/Malbec%2010off_Web%20Mobil.jpg", altText: 'Blend' },
     ];
     return (
-        <div>
+        <div className="bg-base-200">
             <NavBar />
             <SliderAuto items={carouselItems} />
+            <SiliderMobile/>
             <Footer/>
         </div>
     )
