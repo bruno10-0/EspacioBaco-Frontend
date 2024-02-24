ebb6d2c582d81227a85890d5ea6012bba84a48c0
Bruno <brunobrodon75@gmail.com>
2024-02-24 01:35:29 -0300

    feat: :sparkles: Agregamos paginacion para los productos! Gran avance :D

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'; // Importa Link desde React Router
 
 export const Card = ({ product }) => {
   return (
-    <Link to="/#" className="bg-base-100 shadow-lg p-2 cursor-pointer" style={{ textDecoration: 'none' }}>
+    <Link to="/#" className="bg-base-100 rounded-badge shadow-lg p-2 cursor-pointer" style={{ textDecoration: 'none' }}>
       <div className="flex justify-center items-center" style={{ height: "220px", flexShrink: "0" }}>
         <img src={product.image} alt={product.name} className="max-w-full max-h-full object-contain object-center" />
       </div>
@@ -15,7 +15,7 @@ export const Card = ({ product }) => {
         <h2 className="text-center" style={{ letterSpacing: "2px", fontSize: "13px" }}>
           {product.description}
         </h2>
-        <h3 className="text-gray-400" style={{ letterSpacing: "2px", fontSize: "12px" }}>
+        <h3 className="text-accent" style={{ letterSpacing: "2px", fontSize: "12px" }}>
           {product.price}
         </h3>
       </div>
---
+++ src/components/pagination/pagination.jsx
@@ -0,0 +1,60 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+import { FaCaretLeft, FaCaretRight } from "react-icons/fa";
+export const Pagination = ({ pagina, setPagina, maximo }) => {
+    const [input, setInput] = useState(1)
+
+    const prevPage = () => {
+        setInput(parseInt(input) - 1)
+        setPagina(parseInt(input) - 1)
+    }
+    const nextPage = () => {
+        setInput(parseInt(input) + 1)
+        setPagina(parseInt(input) + 1)
+    }
+    const oneKeyDown = (e) => {
+        if (e.keyCode == 13) {
+            setPagina(parseInt(e.target.value))
+            if (
+                parseInt(e.target.value) < 1 ||
+                parseInt(e.target.value) > Math.ceil(maximo) ||
+                isNaN(parseInt(e.target.value))) {
+                setPagina(1);
+                setInput(1);
+            } else {
+                setPagina(parseInt(e.target.value))
+            }
+        }
+    }
+    const onChange = (e) => {
+        setInput(e.target.value);
+    }
+    return (
+        <div className="flex items-center justify-center gap-4 p-2">
+            <button
+                onClick={prevPage}
+                disabled={pagina === 1 || pagina < 1}
+                className="disabled:hidden text-secondary"
+            >
+                <FaCaretLeft className="text-2xl" />
+            </button>
+
+            <input
+                onChange={e => onChange(e)}
+                onKeyDown={e => oneKeyDown(e)}
+                name="page" autoComplete="off"
+                value={input}
+                className="bg-transparent border-secondary border rounded-badge w-12 text-center px-1"
+                type="text" />
+            <p className="flex">de <span className="mx-2">{Math.ceil(maximo)}</span></p>
+            <button
+                onClick={nextPage}
+                disabled={pagina === Math.ceil(maximo) || pagina > maximo}
+                className="disabled:hidden text-secondary"
+            >
+                <FaCaretRight className="text-secondary text-2xl" />
+            </button>
+        </div>
+
+    )
+}
