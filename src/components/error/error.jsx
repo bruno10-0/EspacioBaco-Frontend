e4ca078dd604839d1e92b75b4d98ad9ae6a70d6e
Bruno <brunobrodon75@gmail.com>
2024-02-24 13:38:13 -0300

    style: :lipstick: agregamos una propiedad para probar el responsive

--- src/components/error/error.jsx
+++ src/components/error/error.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom"
 export const Error = () => {
     return (
         <div>
-            <div className="body-error bg-base-300 w-full h-screen flex flex-col justify-center text-center items-center gap-4">
+            <div className="relative body-error bg-base-300 w-full h-screen flex flex-col justify-center text-center items-center gap-4">
                 <h1 className="text-error text-5xl">404</h1>
                 <h2 style={{ letterSpacing: "1px" }}>La página que estás buscando no se encontró</h2>
                 <button>
-                    <Link className="btn bg-primary text-base-100 font-light shadow-md border-none"  to="/" style={{ letterSpacing: "4px" }}>Volver al inicio</Link>
+                    <Link className="btn bg-primary text-base-100 font-light shadow-md border-none" to="/" style={{ letterSpacing: "4px" }}>Volver al inicio</Link>
                 </button>
                 <div className="w-full bg-primary text-center text-base-100 absolute bottom-0 p-2" style={{ letterSpacing: "2px", fontSize: "11px" }}>
                     <p>Es posible que la página no exista, esté en mantenimiento o se encuentre en proceso de mejoras. Por favor, inténtalo nuevamente más tarde.</p>
