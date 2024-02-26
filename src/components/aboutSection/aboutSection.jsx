6bcc9a6f6f2fcdda3ed590ac6af352e6d2c9586c
bruno10-0 <brunobrodon75@gmail.com>
2024-02-25 23:24:35 -0300

    style: :lipstick: terminamos las 3 cards pendiendes de "Nosotros"

--- src/components/aboutSection/aboutSection.jsx
+++ src/components/aboutSection/aboutSection.jsx
@@ -7,13 +7,21 @@ export const AboutSection = () => {
     <section className="about" style={{ marginTop: "70px" }}>
       <div className="md:flex gap-2">
         <div className="text-box md:w-8/12 lg:w-1/2">
-          <h1 className="animation">Dream drops</h1>
-          <h1 className="animation text-primary">Drink, Refresh, Repeat</h1>
+          <h1 className="animation">WinesEli</h1>
+          <h1 className="animation text-primary">
+            Descubre el sabor en cada botella.
+          </h1>
           <p className="animation">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
-            voluptas porro, at quod aliquam nisi esse aut officiis ratione.
-            Deserunt commodi asperiores culpa eaque doloremque doloribus libero
-            alias illum accusamus!
+            En el vasto universo de experiencias que nos regala la vida, hay
+            algo mágico en la forma en que el vino teje sus historias entre las
+            viñas y las barricas. Cada botella cuenta una narrativa única, una
+            fusión de tierra, sol y cuidado meticuloso. Así como el amor
+            transforma el alma y la vida nos brinda lecciones invaluables, el
+            vino nos invita a sumergirnos en su complejidad, a deleitarnos con
+            sus matices y a celebrar cada momento con intensidad. En cada copa,
+            encontramos una oportunidad para conectar, para compartir, para
+            celebrar la belleza efímera de la existencia. Como el buen vino, la
+            vida es para ser saboreada con pasión y disfrutada en cada sorbo.
           </p>
           <button
             type="button"
--- src/components/aboutSection/purpose.jsx
+++ src/components/aboutSection/purpose.jsx
@@ -1,37 +1,51 @@
 import React from "react";
-import { MdConstruction } from "react-icons/md";
+import { LiaEyeSolid } from "react-icons/lia";
+import { GiSpaceShuttle,GiShakingHands } from "react-icons/gi";
 export const Purpose = () => {
   return (
     <div className="carousel rounded-badge h-auto md:justify-center w-full my-5 p-2 gap-2">
       <div
-        className="text-center carousel-item flex-col bg-violet-300 rounded-badge"
+        className="flex items-center text-center carousel-item flex-col glass rounded-badge"
         style={{ width: "300px", height: "350px" }}
       >
-        <h1 className="text-primary text-2xl">Vision</h1>
-        <div className="flex">
-          <h2>En desarrollo</h2>
-          <MdConstruction></MdConstruction>
-        </div>
+        <LiaEyeSolid className="my-2 text-7xl text-primary" />
+        <h1 className="uppercase text-lg mb-2" style={{ letterSpacing: "2px" }}>
+          VISIÓN
+        </h1>
+        <p className="text-start mx-4">
+          Ser líderes globales en vinos, brindando experiencias excepcionales y
+          enriquecedoras que redefinan el placer de disfrutar del vino.
+        </p>
       </div>
       <div
-        className="text-center carousel-item flex-col bg-violet-300 rounded-badge"
+        className="flex items-center text-center carousel-item flex-col glass rounded-badge"
         style={{ width: "300px", height: "350px" }}
       >
-        <h1 className="text-primary  text-2xl">Mision</h1>
-        <div className="flex">
-          <h2>En desarrollo</h2>
-          <MdConstruction></MdConstruction>
-        </div>
+        <GiSpaceShuttle className="my-2 text-7xl text-primary" />
+        <h1 className="uppercase text-lg mb-2" style={{ letterSpacing: "2px" }}>
+          MISIÓN
+        </h1>
+        <p className="text-start mx-4">
+          Ser el destino preferido para los amantes del vino, ofreciendo
+          calidad, servicio excepcional y experiencias únicas que inspiren y
+          deleiten a nuestros clientes.
+        </p>
       </div>
       <div
-        className="text-center carousel-item flex-col bg-violet-300 rounded-badge"
+        className="flex items-center text-center carousel-item flex-col glass rounded-badge"
         style={{ width: "300px", height: "350px" }}
       >
-        <h1 className="text-primary  text-2xl">Valores</h1>
-        <div className="flex">
-          <h2>En desarrollo</h2>
-          <MdConstruction></MdConstruction>
-        </div>
+        <GiShakingHands className="my-2 text-7xl text-primary" />
+        <h1 className="uppercase text-lg mb-2" style={{ letterSpacing: "2px" }}>
+          VALORES
+        </h1>
+       
+        <p className="text-start mx-4">
+          En nuestra tienda nos basamos en valores clave: <span style={{fontWeight:"600", fontStyle:"italic"}}>respeto, autenticidad,
+          responsabilidad y excelencia.</span> Estos principios nos guían
+          hacia la creación de experiencias únicas y enriquecedoras para
+          nuestros clientes.
+        </p>
       </div>
     </div>
   );
