739f5cc485e29b13d31e17721093f07279e5ac18
bruno10-0 <brunobrodon75@gmail.com>
2024-04-02 01:00:19 -0300

    feat: :lipstick: Implementamos el crud de imagens de el eslider de manera sactisfactoria

--- src/components/common/sliderAuto/sliderAuto.jsx
+++ src/components/common/sliderAuto/sliderAuto.jsx
@@ -1,49 +1,72 @@
 /* eslint-disable react/prop-types */
-import "./sliderAuto.css"
-import { useState, useEffect } from 'react';
+import "./sliderAuto.css";
+import { useState, useEffect } from "react";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa6";
+import { useContexto } from "../../../context/Context";
 
-export const SliderAuto = ({ items }) => {
-    const [currentIndex, setCurrentIndex] = useState(0);
+export const SliderAuto = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const { publicaciones } = useContexto();
 
-    const nextSlide = () => {
-        setCurrentIndex((prevIndex) => (prevIndex + 1) % items.length);
-    };
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % publicaciones.length);
+  };
 
-    const prevSlide = () => {
-        setCurrentIndex((prevIndex) => (prevIndex - 1 + items.length) % items.length);
-    };
-
-    useEffect(() => {
-        const interval = setInterval(nextSlide, 7000);
-        return () => clearInterval(interval);
-    }, []);
-    return (
-        <div className="Carousel mt-16 md:mt-28 -mb-5">
-
-            <button className="btn z-10 p-2 rounded-r-badge rounded-l-none bg-base-100  Carousel-button Carousel-button-left" onClick={prevSlide}>
-                <FaChevronLeft className='text-primary text-2xl ml-4' />
-            </button>
-
-            <div className="Carousel-inner w-full h-full" style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
-                {items.map((item, index) => (
-                    <div key={index} className="Carousel-item w-full h-full">
-                        {/* Imagen para pantallas mayores a 780px */}
-                        <img src={item.imageUrlLarge} alt={item.altText} className="w-full h-full object-fill lg:block hidden" />
-
-                        {/* Imagen para pantallas menores o iguales a 780px */}
-                        <img src={item.imageUrlSmall} alt={item.altText} className="w-full h-full object-fill lg:hidden" />
-                    </div>
-
-                ))}
-            </div>
-
-            <button className="btn z-10 p-2 rounded-l-badge rounded-r-none bg-base-100 Carousel-button Carousel-button-right" onClick={nextSlide}>
-                <FaChevronRight className='text-primary text-2xl mr-4' />
-            </button>
-            <div className="absolute -bottom-2 border-none" style={{ width:"100vw",height: "100px" }}>
-                <div className="w-full h-full bg-gradient-to-t from-base-100 to-transparent blur-sm"></div>
-            </div>
-        </div>
+  const prevSlide = () => {
+    setCurrentIndex(
+      (prevIndex) =>
+        (prevIndex - 1 + publicaciones.length) % publicaciones.length
     );
+  };
+
+  useEffect(() => {
+    const interval = setInterval(nextSlide, 7000);
+    return () => clearInterval(interval);
+  }, []);
+  return (
+    <div className="Carousel mt-16 md:mt-28 -mb-5">
+      <button
+        className="btn z-10 p-2 rounded-r-badge rounded-l-none bg-base-100  Carousel-button Carousel-button-left"
+        onClick={prevSlide}
+      >
+        <FaChevronLeft className="text-primary text-2xl ml-4" />
+      </button>
+
+      <div
+        className="Carousel-inner w-full h-full"
+        style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+      >
+        {publicaciones.map((item, index) => (
+          <div key={index} className="Carousel-item w-full h-full">
+            {/* Imagen para pantallas mayores a 780px */}
+            <img
+              src={item.secureURL2}
+              alt={item.titulo}
+              className="w-full h-full object-fill lg:block hidden"
+            />
+
+            {/* Imagen para pantallas menores o iguales a 780px */}
+            <img
+              src={item.secureURL1}
+              alt={item.titulo}
+              className="w-full h-full object-fill lg:hidden"
+            />
+          </div>
+        ))}
+      </div>
+
+      <button
+        className="btn z-10 p-2 rounded-l-badge rounded-r-none bg-base-100 Carousel-button Carousel-button-right"
+        onClick={nextSlide}
+      >
+        <FaChevronRight className="text-primary text-2xl mr-4" />
+      </button>
+      <div
+        className="absolute -bottom-2 border-none"
+        style={{ width: "100vw", height: "100px" }}
+      >
+        <div className="w-full h-full bg-gradient-to-t from-base-100 to-transparent blur-sm"></div>
+      </div>
+    </div>
+  );
 };
--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -1,35 +1,12 @@
-import {NavBar} from "../common/navBar/navBar"
+import { NavBar } from "../common/navBar/navBar";
 import { Footer } from "../common/footer/footer";
 import { SliderAuto } from "../common/sliderAuto/sliderAuto";
 import { SiliderMobile } from "../common/siliderMobile/siliderMobile";
 export const Home = () => {
-  const carouselItems = [
-    {
-      imageUrlLarge:
-        "https://www.solyvinomendoza.com/img/cms/Blend%20Tintos_Web%20pc.jpg",
-      imageUrlSmall:
-        "https://www.solyvinomendoza.com/img/cms/Blend%20Tintos_Web%20Mobil.jpg",
-      altText: "VinoMalbec",
-    },
-    {
-      imageUrlLarge:
-        "https://www.solyvinomendoza.com/img/cms/Promos%20Carnaval_Web%20pc.jpg",
-      imageUrlSmall:
-        "https://www.solyvinomendoza.com/img/cms/Promos%20Carnaval_Web%20Mobil.jpg",
-      altText: "PromoCarnaval",
-    },
-    {
-      imageUrlLarge:
-        "https://www.solyvinomendoza.com/img/cms/Malbec%2010off_Web%20pc.jpg",
-      imageUrlSmall:
-        "https://www.solyvinomendoza.com/img/cms/Malbec%2010off_Web%20Mobil.jpg",
-      altText: "Blend",
-    },
-  ];
   return (
     <div className="bg-base-200">
       <NavBar />
-      <SliderAuto items={carouselItems} />
+      <SliderAuto />
       <SiliderMobile />
       <Footer />
     </div>
