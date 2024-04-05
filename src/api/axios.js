b2623934c6528a7d593b3b9e1d4ce2e82842dc15
bruno10-0 <brunobrodon75@gmail.com>
2024-04-05 01:42:05 -0300

    feat: :sparkles: Implementamos nuevos campos al registro de un producto

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL:'http://localhost:3000/',  /*https://espaciobacobackend.onrender.com*/
+    baseURL:'https://espaciobacobackend.onrender.com',  /*http://localhost:3000/*/
     withCredentials:true
 })
 
