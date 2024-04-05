5c8fa04c4f861afd104d8e2469106794c969843a
bruno10-0 <brunobrodon75@gmail.com>
2024-04-05 03:15:36 -0300

    style: :lipstick: Agregamos referencias de los contribuidores en el footer

--- src/components/common/footer/footer.jsx
+++ src/components/common/footer/footer.jsx
@@ -16,13 +16,42 @@ export const Footer = () => {
           <p>Copyright © 2024 - Todos los derechos resevados</p>
         </aside>
         <nav>
-          <p className="font-semibold">Nuestro Instagram</p>
+          <p className="font-semibold -mt-4 ">Visita nuestro Instagram</p>
           <div className="grid grid-flow-col gap-4">
             <a className="cursor-pointer">
               <PiInstagramLogoFill className="text-white text-3xl" />
             </a>
           </div>
         </nav>
+        <div className="w-full flex justify-center gap-4 -mt-6 font-semibold">
+          <div>
+            <h1 style={{ letterSpacing: "2px" }} className="uppercase text-xs">
+              Desarrollador:
+            </h1>
+            <a
+              href="https://brunodev-zn28.onrender.com/"
+              className="link"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              BrunoDev
+            </a>
+          </div>
+          <div></div>
+          <div>
+            <h1 style={{ letterSpacing: "2px" }} className="uppercase text-xs">
+              Diseñador de iconos y logos:
+            </h1>
+            <a
+              href=""
+              className="link"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Seju1000
+            </a>
+          </div>
+        </div>
       </footer>
     </>
   );
